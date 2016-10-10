67d6fd61f3cfb90d11a98ce1735efcbc3c629a72
Svenn-Arne Dragly <s@dragly.com>
2016-10-10 18:33:19 +0200

    Ensure numbers are double before passing to QParameter
    Otherwise we might risk having ints sent, which breaks the
    visualization.

--- src/shadernode.cpp
+++ src/shadernode.cpp
@@ -79,6 +79,13 @@ void ShaderNode::handlePropertyChange(int index)
     if(m_propertyTypeNames.contains(index) && metaObject()->propertyCount() > index) {
         QMetaProperty metaProperty = metaObject()->property(index);
         QVariant value = metaProperty.read(this);
+        if(value.type() == QVariant::Int) {
+            // Var values may be parsed as int by QML
+            // but we need to treat them as floating point numbers
+            // TODO make this into a new type to make sure we cannot mistakenly forget to convert it
+            // One option is to use phantom types: https://youtu.be/ojZbFIQSdl8?t=24m7s
+            value = QVariant(double(value.toInt()));
+        }
         QString oldTypeName = m_propertyTypeNames[index];
         QString newTypeName = value.typeName();
         if(oldTypeName != newTypeName) {
@@ -199,6 +206,12 @@ bool ShaderNode::setup(ShaderBuilder* shaderBuilder, QString tempIdentifier)
 
         if(!value.isValid()) {
             value = QVariant(0.0);
+        }
+
+        if(value.type() == QVariant::Int) {
+            // Var values may be parsed as int by QML
+            // but we need to treat them as floating point numbers
+            value = QVariant(double(value.toInt()));
         }
 
         m_propertyTypeNames[i] = value.typeName();
