616bce040f23433cc08500fd9f56d67d5dbb1392
Svenn-Arne Dragly <s@dragly.com>
2016-10-13 12:49:03 +0200

    Fix typo vec2 not available for scalar conversion

--- src/shaderutils.cpp
+++ src/shaderutils.cpp
@@ -259,7 +259,7 @@ QString ShaderUtils::convert(const QString &sourceType, const QString &targetTyp
         {"uint", "uint(" + v + ")"},
         {"float", "float(" + v + ")"},
         {"double", "double(" + v + ")"},
-        {"vec3", "vec2(" + v + ", " + v + ")"},
+        {"vec2", "vec2(" + v + ", " + v + ")"},
         {"vec3", "vec3(" + v + ", " + v + ", " + v + ")"},
         {"vec4", "vec4(" + v + ", " + v + ", " + v + ", 1.0)"}
     };
